d4ec6eb6db26e83f3af7fd48128740c6b92d05a8
Nikita <77041440+nkazantsevlez@users.noreply.github.com>
2021-03-13 23:35:51 +0800

    added the ability to select one of four cities

--- src/main/java/AppUI.java
+++ src/main/java/AppUI.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import static org.jsoup.Jsoup.connect;
+
 public class AppUI
 {
 
@@ -15,31 +17,93 @@ public class AppUI
     private JButton UpdateButton;
     private JLabel NowValues;
     private JLabel SensationValue;
+    private JComboBox CityList;
 
     public AppUI() throws IOException {
-        //System.out.println("Погода в Москве: ");
 
-        Document doc=null;
-
-        doc= (Document) Jsoup.connect("https://www.gismeteo.ru/weather-moscow-4368/").get();
-
-       // System.out.println("Сейчас: ");
-        Elements spans=doc.select("span [class=js_value tab-weather__value_l]");
-        Element span=spans.get(0);
-        // System.out.println(span.text());
-
-        //System.out.println("По ощущению: ");
-        Elements spans2=doc.select("span [class=unit unit_temperature_c] ");
-        Element span2=spans2.get(0);
-        //System.out.println(span2.text());
-
-
-        UpdateButton.addActionListener(new ActionListener() {
+        UpdateButton.addActionListener(new ActionListener()
+        {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                NowValues.setText(span.text());
-                SensationValue.setText(span2.text());
+            public void actionPerformed(ActionEvent e)
+            {
+                Document doc=null;
+                Elements spans, spans2;
+                Element span,span2;
 
+                switch(CityList.getSelectedIndex())
+                {
+
+
+                    case 0:
+
+                        try {
+                            doc= (Document) Jsoup.connect("https://www.gismeteo.ru/weather-moscow-4368/").get();
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+
+                        spans=doc.select("span [class=js_value tab-weather__value_l]");
+                        span=spans.get(0);
+
+                        spans2=doc.select("span [class=unit unit_temperature_c] ");
+                        span2=spans2.get(0);
+
+                        NowValues.setText(span.text());
+                        SensationValue.setText(span2.text());
+
+
+                        break;
+
+                    case 1:
+                        try {
+                            doc= (Document) Jsoup.connect("https://www.gismeteo.ru/weather-sankt-peterburg-4079/").get();
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                        spans=doc.select("span [class=js_value tab-weather__value_l]");
+                        span=spans.get(0);
+
+                        spans2=doc.select("span [class=unit unit_temperature_c] ");
+                        span2=spans2.get(0);
+
+                        NowValues.setText(span.text());
+                        SensationValue.setText(span2.text());
+                    break;
+
+                    case 2:
+                        try {
+                            doc= (Document) Jsoup.connect("https://www.gismeteo.ru/weather-novosibirsk-4690/").get();
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                        spans=doc.select("span [class=js_value tab-weather__value_l]");
+                        span=spans.get(0);
+
+                        spans2=doc.select("span [class=unit unit_temperature_c] ");
+                        span2=spans2.get(0);
+
+                        NowValues.setText(span.text());
+                        SensationValue.setText(span2.text());
+                     break;
+
+                    case 3:
+                        try {
+                            doc= (Document) Jsoup.connect("https://www.gismeteo.ru/weather-vladivostok-4877/").get();
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                        spans=doc.select("span [class=js_value tab-weather__value_l]");
+                        span=spans.get(0);
+
+                        spans2=doc.select("span [class=unit unit_temperature_c] ");
+                        span2=spans2.get(0);
+
+                        NowValues.setText(span.text());
+                        SensationValue.setText(span2.text());
+                        break;
+
+                    default:JOptionPane.showMessageDialog(null,"Ошибка!","Ошибка",JOptionPane.ERROR_MESSAGE); break;
+                }
             }
         });
     }
